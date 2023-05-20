c7884a3dde21cc83c4046ac585c00bf6b03cd3de
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-20 19:18:35 +0000

    added basic http

---
+++ server/src/http/mod.rs
@@ -0,0 +1,19 @@
+use axum::{routing::get, Extension, Router};
+use sqlx::PgPool;
+
+pub fn app(db: PgPool) -> Router {
+    Router::new()
+        .route("/", get(me_handler))
+        .layer(Extension(db))
+}
+
+pub async fn me_handler() -> String {
+    String::from("Hello")
+}
+
+pub async fn serve(db: PgPool) {
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app(db).into_make_service())
+        .await
+        .unwrap()
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgPoolOptions;
+use std::env;
+
+mod http;
+
+#[tokio::main]
+async fn main() {
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let db = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&db_url)
+        .await
+        .expect("Could not connect to the database");
+
+    http::serve(db).await
 }
