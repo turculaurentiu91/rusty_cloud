38d075083425c1255ca95204fc62ebc9f94488fa
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-21 08:38:31 +0000

    added todo non login

--- server/src/http/auth/login.rs
+++ server/src/http/auth/login.rs
@@ -4,6 +4,8 @@ use interfaces::auth::{LoginRequest, LoginResponse};
 use crate::{helpers::user::User, http::error::Error};
 
 pub async fn handle(body: Json<LoginRequest>) -> Result<Json<LoginResponse>, Error<String>> {
+    todo!("Verify the user credentials before creating the token");
+
     let token = User::new(1, "test", &body.email).to_oken();
 
     Ok(Json(LoginResponse { token }))
