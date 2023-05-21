64a38a67a91cf965911eac473b1aa931c38a9a4e
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-21 09:43:51 +0000

    added login handler

--- server/src/http/auth/login.rs
+++ server/src/http/auth/login.rs
@@ -1,12 +1,39 @@
-use axum::response::Json;
+use axum::{http::StatusCode, response::Json, Extension};
 use interfaces::auth::{LoginRequest, LoginResponse};
+use sqlx::PgPool;
 
 use crate::{helpers::user::User, http::error::Error};
 
-pub async fn handle(body: Json<LoginRequest>) -> Result<Json<LoginResponse>, Error<String>> {
-    todo!("Verify the user credentials before creating the token");
-
-    let token = User::new(1, "test", &body.email).to_oken();
-
-    Ok(Json(LoginResponse { token }))
+pub async fn handle(
+    Extension(db): Extension<PgPool>,
+    body: Json<LoginRequest>,
+) -> Result<Json<LoginResponse>, Error<String>> {
+    sqlx::query!(
+        r#"
+            SELECT 
+                *
+            FROM users
+            WHERE email = $1
+            AND password = crypt($2, password)
+        "#,
+        body.email,
+        body.password,
+    )
+    .fetch_optional(&db)
+    .await
+    .map(|result| {
+        return match result {
+            Some(result) => {
+                let user = User::new(result.id, &result.name, &result.email);
+                Ok(Json(LoginResponse {
+                    token: user.to_token(),
+                }))
+            }
+            None => Err(Error::new(
+                String::from("Unauthorized"),
+                StatusCode::UNAUTHORIZED,
+            )),
+        };
+    })
+    .map_err(sqlx::Error::into)?
 }
