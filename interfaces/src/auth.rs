5c0fc82fe071786102e8a088e7dde11cb183e914
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-21 07:57:08 +0000

    added auth interfaces

---
+++ interfaces/src/auth.rs
@@ -0,0 +1,19 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct LoginResponse {
+    pub token: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct User {
+    pub id: i32,
+    pub email: String,
+    pub name: String,
+}
--- interfaces/src/lib.rs
+++ interfaces/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod auth;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
