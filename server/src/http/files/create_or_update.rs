bf54383dc4598a85e6c4d4674bbff4d83965f15d
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-24 06:37:30 +0000

    added file upload

---
+++ server/src/http/files/create_or_update.rs
@@ -0,0 +1,101 @@
+use std::{fs, io::Write};
+
+use axum::{extract::Multipart, http::StatusCode, Extension};
+use sqlx::PgPool;
+
+use crate::{helpers::user::User, http::error::Error};
+
+pub async fn handle(
+    Extension(db): Extension<PgPool>,
+    user: User,
+    mut body: Multipart,
+) -> Result<StatusCode, Error<String>> {
+    while let Some(field) = body
+        .next_field()
+        .await
+        .map_err(|e| Error::new(e.to_string(), StatusCode::UNPROCESSABLE_ENTITY))?
+    {
+        let file_name = field.name().unwrap_or_default().to_string();
+        let mime_type = field.content_type().unwrap_or_default().to_string();
+        let data = field.bytes().await.unwrap_or_default();
+
+        if file_name.is_empty() || mime_type.is_empty() || data.is_empty() {
+            continue;
+        }
+
+        // Check if the user has a file with this name
+        let existing_file = sqlx::query!(
+            r#"
+                SELECT
+                    *
+                FROM files AS f
+                WHERE f.owner_id = $1
+                AND f.file_name = $2
+            "#,
+            user.id,
+            file_name
+        )
+        .fetch_optional(&db)
+        .await
+        .map_err(sqlx::Error::into)?;
+
+        let file_id = if let Some(existing_file) = existing_file {
+            // File already exists, update metadata in the database
+            sqlx::query!(
+                r#"
+                    UPDATE files
+                    SET 
+                        mime_type = $1, 
+                        size_in_bytes = $2
+                    WHERE id = $3
+                "#,
+                mime_type,
+                data.len() as i64,
+                existing_file.id,
+            )
+            .execute(&db)
+            .await
+            .map_err(sqlx::Error::into)?;
+            existing_file.id
+        } else {
+            // File doesn't exist, insert new file metadata into the database
+            let new_file = sqlx::query!(
+                r#"
+                    INSERT INTO files (owner_id, file_name, mime_type, size_in_bytes)
+                    VALUES ($1, $2, $3, $4)
+                    RETURNING id
+                "#,
+                user.id,
+                file_name,
+                mime_type,
+                data.len() as i64,
+            )
+            .fetch_one(&db)
+            .await
+            .map_err(sqlx::Error::into)?;
+
+            new_file.id
+        };
+
+        let storage_path = format!("./storage/{file_id}");
+
+        // Create the directory if it does not exist
+        if let Some(parent_dir) = std::path::Path::new(&storage_path).parent() {
+            fs::create_dir_all(parent_dir)
+                .map_err(|e| Error::new(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))?;
+        }
+
+        // Open a file handle for writing
+        let mut file = fs::File::create(&storage_path)
+            .map_err(|e| Error::new(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))?;
+
+        // Write the file bytes to disk
+        file.write_all(&data)
+            .map_err(|e| Error::new(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))?;
+
+        // Close the file handle
+        file.flush()
+            .map_err(|e| Error::new(e.to_string(), StatusCode::INTERNAL_SERVER_ERROR))?;
+    }
+    Ok(StatusCode::NO_CONTENT)
+}
--- server/src/http/files/mod.rs
+++ server/src/http/files/mod.rs
@@ -1,7 +1,13 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, put},
+    Router,
+};
 
+mod create_or_update;
 mod index;
 
 pub fn router() -> Router {
-    Router::new().route("/files", get(index::handle))
+    Router::new()
+        .route("/files", get(index::handle))
+        .route("/files", put(create_or_update::handle))
 }
