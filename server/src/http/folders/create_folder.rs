3aa1618ea7b9a697be36fa83b29d71014b277ac2
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-26 17:18:43 +0000

    added check for parent when crating folder

--- server/src/http/folders/create_folder.rs
+++ server/src/http/folders/create_folder.rs
@@ -7,6 +7,31 @@ pub async fn handle(
     user: User,
     body: Json<CreateOrUpdateFolderRequest>,
 ) -> Result<Json<GetFolderResponse>, Error<String>> {
+    //if parent, check if it exists and it's the owner
+    if let Some(parent_id) = body.parent_id {
+        let parent = sqlx::query!(
+            r#"
+                SELECT
+                    *
+                FROM folders
+                WHERE owner_id = $1
+                AND id = $2
+            "#,
+            user.id,
+            parent_id
+        )
+        .fetch_optional(&db)
+        .await
+        .map_err(sqlx::Error::into)?;
+
+        if let None = parent {
+            return Err(Error::new(
+                "Parent folder does not exist".to_owned(),
+                StatusCode::BAD_REQUEST,
+            ));
+        }
+    }
+
     // test if folder with same name exists
     let folder = sqlx::query!(
         r#"
