5d1ea7853d92b643deedc7d3a32086ba70ca426b
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-27 08:42:55 +0000

    added basic http interaction

--- browser/src/main.rs
+++ browser/src/main.rs
@@ -1,5 +1,35 @@
-use leptos::*; 
+use leptos::*;
 
 fn main() {
-    mount_to_body(|cx| view! { cx,  <p class="mt-2 text-red" >"Hello, world!!!!!"</p> }) 
+    mount_to_body(|cx| view! { cx,  <App />})
+}
+
+#[component]
+fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+    let async_data = create_resource(cx, count, |_| async move {
+        reqwest_wasm::get("http://localhost:3000/api")
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap()
+    });
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count.update(|n| *n += 1);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+        <p>
+            {move || match async_data.read(cx) {
+                None => view! { cx, "Loading..." }.into_view(cx),
+                Some(data) => view! { cx, <span>{data}</span> }.into_view(cx),
+            }}
+        </p>
+    }
 }
