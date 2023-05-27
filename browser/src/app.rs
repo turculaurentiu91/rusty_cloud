7b36a4eb30f44bd7aab7b120cfd39399833bbeec
Laurentiu Turcu <turculaurentiu91@gmail.com>
2023-05-27 16:21:16 +0000

    moved the main component in own module

---
+++ browser/src/app.rs
@@ -0,0 +1,40 @@
+use leptos::{ev::SubmitEvent, *};
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    let (email, set_email) = create_signal(cx, String::new());
+    let (password, set_password) = create_signal(cx, String::new());
+
+    let login_action = create_action(cx, |input: &(String, String)| {
+        let (email, password) = input;
+        crate::auth::login(email.clone(), password.clone())
+    });
+
+    let on_submit = move |e: SubmitEvent| {
+        e.prevent_default();
+
+        let email = email.get();
+        let password = password.get();
+
+        login_action.dispatch((email, password));
+    };
+
+    let token = login_action.value();
+
+    view! { cx,
+        <div>
+            <form on:submit=on_submit>
+            <input type="email" value={email} on:input=move |ev| set_email(event_target_value(&ev))/>
+                <input type="password" value={password} on:input=move |ev| {set_password(event_target_value(&ev))} />
+                <button type="submit">"Login"</button>
+            </form>
+            <p>
+            {move || {match token.get() {
+                Some(Ok(token)) => format!("Token: {}", token),
+                Some(Err(e)) => format!("Error: {}", e),
+                None => "Not logged in".to_string(),
+            }}}
+            </p>
+        </div>
+    }
+}
--- browser/src/main.rs
+++ browser/src/main.rs
@@ -1,45 +1,10 @@
-use leptos::{ev::SubmitEvent, *};
+use crate::app::App;
+use leptos::*;
 
+mod app;
 mod auth;
+mod routes;
+
 fn main() {
     mount_to_body(|cx| view! { cx,  <App />})
-}
-
-#[component]
-fn App(cx: Scope) -> impl IntoView {
-    let (email, set_email) = create_signal(cx, String::new());
-    let (password, set_password) = create_signal(cx, String::new());
-
-    let login_action = create_action(cx, |input: &(String, String)| {
-        let (email, password) = input;
-        auth::login(email.clone(), password.clone())
-    });
-
-    let on_submit = move |e: SubmitEvent| {
-        e.prevent_default();
-
-        let email = email.get();
-        let password = password.get();
-
-        login_action.dispatch((email, password));
-    };
-
-    let token = login_action.value();
-
-    view! { cx,
-        <div>
-            <form on:submit=on_submit>
-            <input type="email" value={email} on:input=move |ev| set_email(event_target_value(&ev))/>
-                <input type="password" value={password} on:input=move |ev| {set_password(event_target_value(&ev))} />
-                <button type="submit">"Login"</button>
-            </form>
-            <p>
-            {move || {match token.get() {
-                Some(Ok(token)) => format!("Token: {}", token),
-                Some(Err(e)) => format!("Error: {}", e),
-                None => "Not logged in".to_string(),
-            }}}
-            </p>
-        </div>
-    }
 }
